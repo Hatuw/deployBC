526db859ce949f51beff24612886faf70ce1e653
hatuw <jiaxi_wu@ieee.org>
2018-04-06 18:30:54 +0800

    modify add-repository

--- ethereum/setup.sh
+++ ethereum/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get install software-properties-common
-sudo apt-get-repository -y ppa:ethereum/ethereum
+sudo apt-add-repository -y ppa:ethereum/ethereum
 sudo apt-get update
 sudo apt-get install ethereum
