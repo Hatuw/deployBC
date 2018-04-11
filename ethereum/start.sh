176101369b589395b48477b1ec5dfdfe0866137a
hatuw <jiaxi_wu@ieee.org>
2018-04-11 20:27:50 +0800

    update start.sh

--- ethereum/start.sh
+++ ethereum/start.sh
@@ -1,4 +1,8 @@
-#nohup geth --rpc --rpccorsdomain "*" --bootnodes $(curl https://raw.githubusercontent.com/Hatuw/deployBC/master/ethereum/bootnode) &
+# nohup geth --rpc --rpccorsdomain "*" --bootnodes $(curl https://raw.githubusercontent.com/Hatuw/deployBC/master/ethereum/bootnode) --port 30303 > geth.log &
+
+# geth --rpc --rpccorsdomain "*" --bootnodes $(curl https://raw.githubusercontent.com/Hatuw/deployBC/master/ethereum/bootnode) --port 30303 console
 
 
 ansible eth -m shell -a "nohup geth --rpc --rpccorsdomain \'*\' --bootnodes \$(curl https://raw.githubusercontent.com/Hatuw/deployBC/master/ethereum/bootnode) >> geth.log" -T 1 -f 10
+
+
