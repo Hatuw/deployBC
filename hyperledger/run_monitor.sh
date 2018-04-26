5c0b6f85cd0eec6acc6337a705e15edd6d4ad390
hatuw <jiaxi_wu@ieee.org>
2018-04-26 18:37:31 +0800

    update script

--- hyperledger/run_monitor.sh
+++ hyperledger/run_monitor.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 
 # create a InfluxDB container
-docker run -d -p 8083:8083 -p 8086:8086 --expose 8090 --expose 8099 --name influxsrv -e PRE_CREATE_DB=cadvisor tutum/influxdb:latest
+docker run -d \
+    -p 8083:8083 \
+    -p 8086:8086 \
+    --expose 8090 \
+    --expose 8099 \
+    --name influxsrv \
+    -e PRE_CREATE_DB=cadvisor \
+    tutum/influxdb
 
 # create cAdvisor container and link to InfluxDB container
-docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker:/var/lib/docker:ro --publish=8080:8080 --detach=true --link influxsrv:influxsrv --name=cadvisor google/cadvisor:latest -storage_driver=invluxdb -storage_driver_db=cadvisor -storage_driver_host=influxsrv:8086
+docker run \
+    --volume=/:/rootfs:ro \
+    --volume=/var/run:/var/run:rw \
+    --volume=/sys:/sys:ro \
+    --volume=/var/lib/docker:/var/lib/docker:ro \
+    -p 8080:8080 \
+    --detach=true \
+    --link influxsrv:influxsrv \
+    --name=cadvisor \
+    google/cadvisor:latest \
+    -storage_driver=influxdb \
+    -storage_driver_db=cadvisor \
+    -storage_driver_host=influxsrv:8086
 
 # create Grafana container and lint to InfluxDB container
-docker run -d -p 3000:3000 -e INFLUXDB_HOST=localhost -e INFLUXDB_PORT=8086 -e INFLUXDB_NAME=cadvisor -e INFLUXDB_USER=root -e INFLUXDB_PASS=root --link influxsrv:influxsrv --name grafana grafana/grafana:latest
+docker run -d \
+    -p 3000:3000 \
+    -e INFLUXDB_HOST=localhost \
+    -e INFLUXDB_PORT=8086 \
+    -e INFLUXDB_NAME=cadvisor \
+    -e INFLUXDB_USER=root \
+    -e INFLUXDB_PASS=root \
+    --link influxsrv:influxsrv \
+    --name grafana \
+    grafana/grafana
