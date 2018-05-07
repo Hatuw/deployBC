1535a086d566911861e13775b1567839f4dd83a4
hatuw <jiaxi_wu@ieee.org>
2018-05-07 17:50:25 +0800

    init pydocker

--- hyperledger/pydocker.py
+++ hyperledger/pydocker.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
+import os
 import docker
 
 BASE_URL = "unix://var/run/docker.sock"
+LOGS_DIR = "./logs"
 
 client = docker.DockerClient(base_url=BASE_URL, version='auto', timeout=5)
 
 containers = client.containers.list()
 
 for item in containers:
-    print(item.attrs['Config'])
-    break
-    # print(item.logs())
+    container_name = item.attrs['Name'][1:]
+    print(container_name)
+    logs_data = item.logs().decode('utf-8')
+    out_file = os.path.join(LOGS_DIR, '{}.logs'.format(container_name))
+    with open(out_file, 'w') as logs_out:
+        logs_out.write(logs_data)
