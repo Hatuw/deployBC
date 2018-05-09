eac51bbc08b34c2688828b12730016c5ea4900eb
hatuw <jiaxi_wu@ieee.org>
2018-05-09 18:08:47 +0800

    add piling

--- hyperledger/piling.py
+++ hyperledger/piling.py
@@ -13,6 +13,6 @@ peer_dir = os.path.join(PROJECT_DIR, 'peer')
 go_files = []
 for root, dirs, files in os.walk(peer_dir):
     for file in files:
-        if re.match(r'(\w+[^(_test)]\.go$)', file):
-            print(file)
+        if re.match(r'([A-Za-z0-9]+\.go$)', file):
             fullpath = os.path.join(root, file)
+            print(fullpath)
