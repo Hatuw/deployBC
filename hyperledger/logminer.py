379970f5fb1645c26b7a24c90d1d99b52bf42e63
hatuw <jiaxi_wu@ieee.org>
2018-05-21 16:55:09 +0800

    update logminer

--- hyperledger/logminer.py
+++ hyperledger/logminer.py
@@ -4,17 +4,31 @@ import os
 import re
 
 log_file = './logs/console-logs/orderer.example.com.logs'
+out_log = './logs/filter.log'
 
 assert os.path.exists(log_file), "logs does not exist"
 
-logs_data = []
-with open(log_file, 'r') as f_in:
-    tmp_log = f_in.readlines()
 
-for item in tmp_log:
-    if re.match(r'.+(\[Start\]|\[End\])', item):
-        logs_data.append(item)
-del tmp_log
+def filter_logs(src_log, out_name='./logs/filter.log'):
+    logs_data = []
+    with open(src_log, 'r') as f_in:
+        tmp_log = f_in.readlines()
 
-for i in range(10):
-    print(logs_data[i], end='')
+    for item in tmp_log:
+        if re.match(r'.+(\[Start\]|\[End\])', item):
+            logs_data.append(item)
+    del tmp_log
+
+    with open(out_name, 'w') as f_out:
+        for item in logs_data:
+            f_out.write(item)
+    return True
+
+with open(out_log, 'r') as logs_in:
+    logs_data = []
+    for line in logs_in.readlines():
+        logs_data.append(line.strip())
+
+
+for i in range(5):
+    print(logs_data[i])
